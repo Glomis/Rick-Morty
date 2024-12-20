66945e6f96d9d435a1611eba801809c3f1ce50fc
Митюшин Максим <glpat-qXpKtLWDEp56B6HxKAsx>
2024-12-20 22:34:48 +0300

    Initial Commit

---
+++ Rick&Morty/Rick_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyApp.swift
+//  Rick&Morty
+//
+//  Created by Максим Митюшин on 20.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
